5d4b7a4e398dabac4ccb766e0665b2d340df4dbd
xw <1162744696@qq.com>
2023-09-21 10:47:57 +0800

    feat: 线性查找法

--- linear_search.go
+++ linear_search.go
@@ -1,1 +1,14 @@
 package algorithm
+
+type LinearSearch[T comparable] struct {
+}
+
+func (LinearSearch[T]) Search(data []T, target T) int {
+	for i, item := range data {
+		if item == target {
+			return i
+		}
+	}
+
+	return -1
+}
---
+++ linear_search_test.go
@@ -0,0 +1,12 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLinearSearch(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5}
+	target := 3
+	fmt.Println(LinearSearch[int]{}.Search(data, target))
+}
